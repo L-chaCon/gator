5f0cdf9a38d730ce72f4f64b9fb98354caaba7e6
Octavio Montt <montt.octavio@gmail.com>
2025-08-16 13:30:40 +0100

    feat: middleware

--- handler_feeds.go
+++ handler_feeds.go
@@ -9,14 +9,9 @@ import (
 	"github.com/google/uuid"
 )
 
-func handlerAddFeed(s *state, cmd command) error {
+func handlerAddFeed(s *state, cmd command, user database.User) error {
 	if len(cmd.Args) != 2 {
 		return fmt.Errorf("usage: %s <name> <url>", cmd.Name)
-	}
-
-	currentUser, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
-	if err != nil {
-		return errors.New("user not found")
 	}
 
 	newFeed, err := s.db.CreateFeed(
@@ -25,7 +20,7 @@ func handlerAddFeed(s *state, cmd command) error {
 			ID:     uuid.New(),
 			Name:   cmd.Args[0],
 			Url:    cmd.Args[1],
-			UserID: currentUser.ID,
+			UserID: user.ID,
 		},
 	)
 	if err != nil {
@@ -36,7 +31,7 @@ func handlerAddFeed(s *state, cmd command) error {
 		context.Background(),
 		database.CreateFeedFollowParams{
 			ID:     uuid.New(),
-			UserID: currentUser.ID,
+			UserID: user.ID,
 			FeedID: newFeed.ID,
 		})
 	if err != nil {
@@ -63,55 +58,6 @@ func handlerGetFeeds(s *state, cmd command) error {
 	return nil
 }
 
-func handlerFollow(s *state, cmd command) error {
-	if len(cmd.Args) != 1 {
-		return fmt.Errorf("usage: %s", cmd.Name)
-	}
-
-	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
-	if err != nil {
-		return errors.New("no user found")
-	}
-
-	feed, err := s.db.GetFeed(context.Background(), cmd.Args[0])
-	if err != nil {
-		return errors.New("no feed found")
-	}
-
-	follow, err := s.db.CreateFeedFollow(
-		context.Background(),
-		database.CreateFeedFollowParams{
-			ID:     uuid.New(),
-			UserID: user.ID,
-			FeedID: feed.ID,
-		})
-	if err != nil {
-		return fmt.Errorf("not able to follow feed: %w", err)
-	}
-	printFollow(follow)
-
-	return nil
-}
-
-func handlerFollowing(s *state, cmd command) error {
-	if len(cmd.Args) != 0 {
-		return fmt.Errorf("usage: %s", cmd.Name)
-	}
-
-	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
-	if err != nil {
-		return errors.New("not able to found current user")
-	}
-
-	following, err := s.db.GetFeedFollowsForUser(context.Background(), user.ID)
-	if err != nil {
-		return errors.New("not able to found following")
-	}
-	printFollowing(following)
-
-	return nil
-}
-
 func printFeed(feed database.Feed) {
 	fmt.Printf(" * ID:      %v\n", feed.ID)
 	fmt.Printf(" * Name:    %v\n", feed.Name)
@@ -134,24 +80,6 @@ func printFeedList(feeds []database.GetFeedsRow) error {
 	}
 	for _, feed := range feeds {
 		printFeedUserName(feed)
-	}
-	return nil
-}
-
-func printFollow(feedFollow database.CreateFeedFollowRow) {
-	fmt.Printf(" * ID:           %v\n", feedFollow.ID)
-	fmt.Printf(" * User name:    %v\n", feedFollow.UserName)
-	fmt.Printf(" * Feed name:    %v\n", feedFollow.FeedName)
-	fmt.Println("=====================================")
-}
-
-func printFollowing(following []database.GetFeedFollowsForUserRow) error {
-	if len(following) == 0 {
-		return errors.New("no feeds followed")
-	}
-	for _, follow := range following {
-		fmt.Printf(" * Feed name:   %v\n", follow.Name)
-		fmt.Printf(" * URL name:    %v\n", follow.Url)
 	}
 	return nil
 }
